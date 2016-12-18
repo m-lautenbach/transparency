eb429864b8123558fdce5f2cbc8d33cc4cc9d2e8
Markus Lautenbach <m.lautenbach@itsonix.eu>
2016-12-18 16:44:57 +0800

    Add npm resolution to rollup

--- rollup-client.config.js
+++ rollup-client.config.js
@@ -1,4 +1,9 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+
 export default {
+  plugins: [ nodeResolve() ],
+  sourceMap: true,
   entry: 'lib/client.js',
   dest: 'public/js/index.js',
+  format: 'cjs'
 };
--- rollup-server.config.js
+++ rollup-server.config.js
@@ -1,4 +1,9 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+
 export default {
+  plugins: [ nodeResolve() ],
+  sourceMap: true,
   entry: 'lib/server.js',
   dest: './index.js',
+  format: 'cjs'
 };
