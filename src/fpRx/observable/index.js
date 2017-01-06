1635bc47ef7b850c7bce3332f7a012027ccf3fef
Markus Lautenbach <m.lautenbach@itsonix.eu>
2017-01-06 19:43:31 +0100

    Wrap methods to fp functions

--- src/fpRx/observable/index.js
+++ src/fpRx/observable/index.js
@@ -1,5 +1,5 @@
 import {Observable} from 'rxjs'
-import {curry, ary, flip, flow} from 'lodash/fp'
+import {bind, curry, ary, flip, flow} from 'lodash/fp'
 
 const bindTwo = curry((f, arg2, arg1) =>
   f(arg1, arg2)
@@ -9,12 +9,20 @@ const bindThree = curry((f, arg3, arg2, arg1) =>
   f(arg1, arg2, arg3)
 )
 
+const bindMethodOne = curry((methodName, arg, object) =>
+  object[methodName](arg)
+)
+
 const of = Observable.of
+const startWith = bindMethodOne('startWith')
+const scan = bindMethodOne('scan')
 const fromEvent = bindTwo(Observable.fromEvent)
 const combineLatest = bindThree(Observable.combineLatest)
 
 export {
+  combineLatest,
   fromEvent,
   of,
-  combineLatest,
+  scan,
+  startWith,
 }
