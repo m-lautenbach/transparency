18667c147f48578d516a50bba6b0fc06932a6b15
Markus Lautenbach <m.lautenbach@itsonix.eu>
2017-01-14 00:54:10 +0100

    Persist client connections to database
    Missing:
    Client should be cleared or marked as disconnected on server shutdown.
    Server should also listen to client connections from database and not aggregate himself.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import {
   flow,
   get,
   map,
+  merge,
   omit,
 } from 'lodash/fp'
 import r from 'rethinkdb'
@@ -27,80 +28,105 @@ function getSocketDetails(socket) {
 function app(ioServer) {
   
   let connection = null;
-  r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+  r.connect({ host: 'localhost', port: 28015 }, function (err, conn) {
     if (err) throw err;
-    r.db('transparency').tableCreate('clients').run(conn, function(err, result) {
-      if (err) throw err;
-      console.log(JSON.stringify(result, null, 2));
+    
+    r.db('transparency').tableCreate('clients').run(conn, () => {
+      const masterNS = ioServer.of('/master');
+      
+      const masterSockets = fromEvent('connection', masterNS);
+      
+      const connections = fromEvent('connection', ioServer);
+      
+      const disconnections = flow(
+        flatMap(socket => flow(
+          fromEvent('disconnect'),
+          concat(of(socket)),
+          combineLatest(concat),
+        )(socket))
+      )(connections);
+      
+      const clients = flow(
+        flatMap(
+          socket => flow(
+            fromEvent('client details'),
+            concat(of(socket)),
+            combineLatest(concat),
+          )(socket)
+        ),
+        rxMap(
+          ([socket, clientDetails]) =>
+            assign(
+              clientDetails,
+              getSocketDetails(socket),
+              { socket: socket }
+            )
+        )
+      )(connections);
+
+      clients.subscribe((client) => {
+        r.db('transparency')
+          .table('clients')
+          .insert(
+            merge(
+              client,
+              {status: 'connected'}
+            )
+          )
+          .run(conn, (err) => {
+            if(err) throw err
+          })
+      })
+      
+      disconnections.subscribe(([socket]) => {
+        r.db('transparency')
+          .table('clients')
+          .get(socket.id)
+          .update({
+            status: 'disconnected'
+          })
+          .run(conn, (err) => {
+            if(err) throw err
+          })
+      })
+      
+      const allConnections = toList(clients);
+      const allDisconnections = toList(disconnections);
+      
+      const connectedClients = toCurrentList(
+        'id',
+        of([]),
+        allConnections,
+        rxMap(map(get('0.id')), allDisconnections),
+      );
+      
+      flow(
+        withLatestFrom(connectedClients),
+        subscribe(
+          ([masterSocket, connectedClients]) =>
+            masterSocket.emit('client list',
+              map(
+                omit('socket'),
+                connectedClients,
+              )
+            )
+        )
+      )(masterSockets)
+      
+      flow(
+        rxMap(omit('socket')),
+        subscribe(
+          client => masterNS.emit('client connected', client)
+        )
+      )(clients)
+      
+      subscribe(
+        ([socket, msg]) => masterNS.emit('client disconnected', socket.id),
+        disconnections
+      )
+      
     })
   })
-  
-  const masterNS = ioServer.of('/master');
-  
-  const masterSockets = fromEvent('connection', masterNS);
-  
-  const connections = fromEvent('connection', ioServer);
-  
-  const disconnections = flow(
-    flatMap(socket => flow(
-      fromEvent('disconnect'),
-      concat(of(socket)),
-      combineLatest(concat),
-    )(socket))
-  )(connections);
-  
-  const clients = flow(
-    flatMap(
-      socket => flow(
-        fromEvent('client details'),
-        concat(of(socket)),
-        combineLatest(concat),
-      )(socket)
-    ),
-    rxMap(
-      ([socket, clientDetails]) =>
-        assign(
-          clientDetails,
-          getSocketDetails(socket),
-          { socket: socket }
-        )
-    )
-  )(connections);
-  
-  const allConnections = toList(clients);
-  const allDisconnections = toList(disconnections);
-  
-  const connectedClients = toCurrentList(
-    'id',
-    of([]),
-    allConnections,
-    rxMap(map(get('0.id')), allDisconnections),
-  );
-  
-  flow(
-    withLatestFrom(connectedClients),
-    subscribe(
-      ([masterSocket, connectedClients]) =>
-        masterSocket.emit('client list',
-          map(
-            omit('socket'),
-            connectedClients,
-          )
-        )
-    )
-  )(masterSockets)
-  
-  flow(
-    rxMap(omit('socket')),
-    subscribe(
-      client => masterNS.emit('client connected', client)
-    )
-  )(clients)
-  
-  subscribe(
-    ([socket, msg]) => masterNS.emit('client disconnected', socket.id),
-    disconnections
-  )
 }
 
 export default app
