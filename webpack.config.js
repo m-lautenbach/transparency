47c1ae93933392899c74fc24a12255252d3ed121
Markus Lautenbach <m.lautenbach@itsonix.eu>
2017-01-15 21:29:42 +0100

    Autoformat

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,53 @@
 const path = require('path');
-var fs = require('fs');
+const fs = require('fs');
 
 const moduleDefinition = {
-    loaders: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015', 'stage-0']
-            }
-        }
-    ]
+  loaders: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'stage-0']
+      }
+    }
+  ]
 }
 
-var nodeModules = {};
+const nodeModules = {};
 fs.readdirSync('node_modules')
-  .filter(function(x) {
+  .filter(function (x) {
     return ['.bin'].indexOf(x) === -1;
   })
-  .forEach(function(mod) {
+  .forEach(function (mod) {
     nodeModules[mod] = 'commonjs ' + mod;
   });
 
 module.exports = [
-    {
-        entry: './src/client.js',
-        devtool: 'source-map',
-        output: {
-            path: path.join(__dirname, 'public', 'js'),
-            filename: 'index.js'
-        },
-        module: moduleDefinition
-    }
-,
-    {
-        context: __dirname,
-        entry: './src/server.js',
-        target: 'node',
-        node: {
-            __filename: false,
-            __dirname: false
-        },
-        externals: nodeModules,
-        devtool: 'source-map',
-        output: {
-            path: __dirname,
-            filename: 'index.js'
-        },
-        module: moduleDefinition
-    }
+  {
+    entry: './src/client.js',
+    devtool: 'source-map',
+    output: {
+      path: path.join(__dirname, 'public', 'js'),
+      filename: 'index.js'
+    },
+    module: moduleDefinition
+  }
+  ,
+  {
+    context: __dirname,
+    entry: './src/server.js',
+    target: 'node',
+    node: {
+      __filename: false,
+      __dirname: false
+    },
+    externals: nodeModules,
+    devtool: 'source-map',
+    output: {
+      path: __dirname,
+      filename: 'index.js'
+    },
+    module: moduleDefinition
+  }
 ]
