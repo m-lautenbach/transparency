054de1230642ef3e1536bf63344031155b43a90e
Markus Lautenbach <m.lautenbach@itsonix.eu>
2017-01-19 19:09:41 +0100

    Simplify

--- src/app.js
+++ src/app.js
@@ -7,66 +7,8 @@ import {
   merge,
   omit,
 } from 'lodash/fp'
-import r from 'rethinkdb'
-import * as Rx from 'rxjs'
-
-function getSocketDetails(socket) {
-  return { socketId: socket.id, address: socket.handshake.address }
-}
 
 function app(ioServer) {
-  
-  let connection = null;
-  r.connect({ host: 'localhost', port: 28015 }, function (err, conn) {
-    if (err) throw err;
-
-    r.db('transparency').tableCreate('events').run(conn, () => {
-      const connections = Rx.Observable.fromEvent(ioServer, 'connection');
-
-      connections.subscribe((socket) => {
-        r.db('transparency')
-          .table('events')
-          .insert(
-            merge(
-              getSocketDetails(socket),
-              {type: 'connected'},
-            )
-          )
-          .run(conn, (err, data) => {
-            if(err) throw err
-            if(data.errors > 0) throw data.first_error
-          })
-      })
-
-      const disconnections =
-        connections
-          .flatMap(socket =>
-            Rx.Observable.combineLatest(
-              Rx.Observable
-                .fromEvent(socket, 'disconnect'),
-              Rx.Observable.of(socket),
-            )
-          )
-
-      disconnections.subscribe(([msg, socket]) => {
-        r.db('transparency')
-          .table('events')
-          .insert(
-            merge(
-              getSocketDetails(socket),
-              {
-                type: 'disconnected',
-                msg,
-              },
-            )
-          )
-          .run(conn, (err, data) => {
-            if(err) throw err
-            if(data.errors > 0) throw data.first_error
-          })
-      })
-    })
-  })
 }
 
 export default app
--- src/pages/client.js
+++ src/pages/client.js
@@ -1,129 +1,37 @@
-import {
-  concat,
-  find,
-  get,
-  identity,
-  takeRightWhile,
-  last,
-  isEmpty,
-  join,
-} from 'lodash/fp'
 import io from 'socket.io-client'
 import { h } from 'virtual-dom'
-import bowser from 'bowser'
 
 import { updateDOM } from '../sinks'
-import * as Rx from 'rxjs'
+import Rx from 'rxjs'
 
 function handler() {
-  const socket = io({ 'forceNew': true });
-  const clientDetails = {
-    browser: {
-      name: bowser.name,
-      version: bowser.version,
-      capabilities: find((flag) => bowser[flag], ['a', 'c', 'x']),
-    },
-    os: find(
-      (flag) => bowser[flag],
-      [
-        'mac',
-        'windows',
-        'windowsphone',
-        'linux',
-        'chromeos',
-        'android',
-        'ios',
-        'blackberry',
-        'firefoxos',
-        'webos',
-        'bada',
-        'tizen',
-        'sailfish'
-      ],
-    ),
-  };
-  socket.emit('client details', clientDetails)
+  const socket = io();
 
   const connects = Rx.Observable
     .fromEvent(socket, 'connect')
-    .map(() => ({
-      type: 'socket',
-      status: 'connected',
-    }))
+    .map(() => 'connected')
 
   const disconnects = Rx.Observable
     .fromEvent(socket, 'disconnect')
-    .map(() => ({
-      type: 'socket',
-      status: 'disconnected',
-    }))
-
-  const popUndoGroup = acc =>
-    acc.slice(0, isEmpty(last(acc)) && -2 || -1).concat([[]])
-
-  const pushEmptyUndoGroup = acc =>
-    isEmpty(last(acc)) && acc || acc.concat([[]])
-
-  const appendCharToLastUndoGroup = char => (acc) =>
-    acc.slice(0, -1).concat(
-      acc.slice(-1).pop().concat(char)
-    )
-
-  const keypresses = Rx.Observable
-    .fromEvent(document, 'keydown')
-
-  const undoPressed = keypresses
-    .filter(event => event.metaKey && event.key === 'z')
-
-  const inputChars = keypresses
-    .filter(event => !event.altKey && !event.metaKey && !event.ctrlKey)
-    .map(get('key'))
-    .filter(key => key.length === 1)
-
-  const input = inputChars
-    .map(appendCharToLastUndoGroup)
-    .merge(
-      undoPressed.map(() => popUndoGroup)
-    )
-    .merge(
-      inputChars
-        .auditTime(1000)
-        .map(() => pushEmptyUndoGroup)
-    )
-    .scan(
-      (acc, f) => f(acc),
-      [[]],
-    )
-    .map(join(''))
-    .distinctUntilChanged()
-
-  Rx.Observable
-    .fromEvent(document, 'click')
+    .map(() => 'disconnected')
 
   const connectionState = Rx.Observable
     .merge(connects, disconnects)
-    .startWith({ type: 'socket', status: 'connecting' })
+    .startWith('connecting')
 
   return Rx.Observable
     .combineLatest(
       connectionState,
-      input.startWith(''),
     )
     .subscribe(
-      ([connectionState, input]) => updateDOM(renderVDOM(connectionState, input))
+      ([connectionState]) => updateDOM(renderVDOM(connectionState))
     )
 }
 
-function renderVDOM(connectionState, input) {
+function renderVDOM(connectionState) {
   return h('div',
     [
-      h('ul', { className: 'nav nav-tabs' }, [
-          h('li', h('a', { href: "javascript:navTo('/master');" }, 'Master')),
-          h('li', { className: 'active' }, h('a', { href: "javascript:;" }, 'Client')),
-        ]
-      ),
-      h('div', input),
-      h('i', { className: `fa fa-circle connection-state ${connectionState.status}` }),
+      h('i', { className: `fa fa-circle connection-state ${connectionState}` }),
     ]
   )
 }
