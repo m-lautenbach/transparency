026c9e7f649d1263b7dbc1a9a76a3589f3dba365
Markus Lautenbach <m.lautenbach@itsonix.eu>
2017-01-27 16:42:09 +0100

    Remove undo experiment

--- src/pages/client.js
+++ src/pages/client.js
@@ -1,13 +1,12 @@
 import io from 'socket.io-client'
 import { h } from 'virtual-dom'
 import {
-  isEqual,
-  last,
   flatten,
   flow,
   get,
   map,
   zip,
+  tap,
 } from 'lodash/fp'
 
 import { updateDOM } from '../sinks'
@@ -28,76 +27,12 @@ function handler() {
     .merge(connects, disconnects)
     .startWith('connecting')
 
-  const inputData = [
-    { id: 'input_a', title: 'Input A' },
-    { id: 'input_b', title: 'Input B' },
-    { id: 'input_c', title: 'Input C' },
-  ]
-
-  const targetIdEqual = id => flow(
-    get('target.id'),
-    isEqual(id),
-  )
-
-  const currentFocus = Rx.Observable.merge(
-    Rx.Observable
-      .fromEvent(document, 'focusout')
-      .map(() => null),
-    Rx.Observable
-      .fromEvent(document, 'focusin')
-      .map(get('target.id')),
-  )
-    .startWith(null)
-
-  const undoPressed = Rx.Observable
-    .fromEvent(document, 'keydown')
-    .filter(event => event.metaKey && event.key === 'z')
-
-  const updateLastUndoGroup = chars => (acc) =>
-    acc.slice(0, -1).concat([chars])
-
-  const hasLastGroupChanged = acc =>
-    acc.length === 1 ||
-      last(acc) !== acc.slice(-2, -1)
-
-  const pushUndoGroup = acc =>
-    hasLastGroupChanged(acc) && acc.concat([last(acc)]) || acc
-
-  const inputs = Rx.Observable.combineLatest(
-    [Rx.Observable.of(inputData)].concat(
-      inputData
-        .map(get('id'))
-        .map(id => {
-          const inputEvents =
-            Rx.Observable
-              .fromEvent(document, 'input')
-              .filter(targetIdEqual(id))
-
-          return inputEvents
-            .map(get('target.value'))
-            .map(updateLastUndoGroup)
-            .merge(
-              inputEvents
-                .auditTime(1000)
-                .map(() => pushUndoGroup)
-            )
-            .scan((acc, f) => f(acc), [])
-            .startWith([])
-        })
-    )
-  ).map(([inputs, ...undoStacks]) =>
-    zip(
-      inputs,
-      undoStacks,
-    ).map(([input, undoGroups]) => {
-      return {...input, value: last(undoGroups)}
-    })
-  )
-
   return Rx.Observable
     .combineLatest(
       connectionState,
-      inputs,
+      (connectionState) => {
+        return {connectionState}
+      }
     )
     .subscribe(flow(
       renderVDOM,
@@ -105,17 +40,10 @@ function handler() {
     ))
 }
 
-function renderVDOM([connectionState, inputs]) {
+function renderVDOM({connectionState}) {
   return h('div',
     [
       h('i', { className: `fa fa-circle connection-state ${connectionState}` }),
-      h('div', { className: 'form-group' }, flow(
-        map(({ id, title, value }) => [
-          h('label', { for: id }, title),
-          h('input', { type: 'text', className: 'form-control', id, value }),
-        ]),
-        flatten,
-      )(inputs))
     ]
   )
 }
